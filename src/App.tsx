59569cda2590ef75d09e871f13953cc1ee63bb8e
Yifei He <yifei@hesyifei.com>
2021-12-16 14:28:19 -0800

    install `react-jsonschema-form` and `bootstrap@3`

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from "@rjsf/core";
+import { JSONSchema7 } from "json-schema";
 import './App.css';
 
 function App() {
+  const schema: JSONSchema7 = {
+    title: "Test form",
+    type: "string"
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form schema={schema} />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
