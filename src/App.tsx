c36d65b9a9bd2aa432b3514dd3829f4cc052558e
Yifei He <yifei@hesyifei.com>
2021-12-16 19:59:18 -0800

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,70 @@ function App() {
   const [formData, setFormData] = React.useState(null);
 
   return (
-    <div className="App">
-      <Form
-        schema={schema}
-        formData={formData}
-        onChange={(e) => setFormData(e.formData)}
-        noHtml5Validate
-      />
+    <>
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#navbar"
+              aria-expanded="false"
+              aria-controls="navbar"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li>
+                <a href="#">Load Study File</a>
+              </li>
+              <li>
+                <a href="#">Save Study File</a>
+              </li>
+              <li>
+                <a href="#">Export for Well Ping</a>
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <a href="#root_studyInfo__title">→ Study Info</a>
+              </li>
+              <li>
+                <a href="#root_streams__title">→ Streams</a>
+              </li>
+              <li>
+                <a href="#">→ Extra Data</a>
+              </li>
+              <li>
+                <a href="#">→ Questions Blocks</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="App">
+          <h1>Well Ping Study File Editor</h1>
+          <p>Please use this editor on a desktop browser.</p>
 
-      <pre>{JSON.stringify(formData, null, 2)}</pre>
-    </div>
+          <Form
+            schema={schema}
+            formData={formData}
+            onChange={(e) => setFormData(e.formData)}
+            noHtml5Validate
+            liveValidate
+          />
+
+          <pre>{JSON.stringify(formData, null, 2)}</pre>
+        </div>
+      </div>
+    </>
   );
 }
 
