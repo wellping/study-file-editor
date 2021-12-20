e5ae60b2fbdf0909ce3d845615b11264a43518b1
Yifei He <yifei@hesyifei.com>
2021-12-20 11:46:46 -0800

    eslint typescript

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+  rules: {
+    "prettier/prettier": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
