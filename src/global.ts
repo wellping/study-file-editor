ed9e1ddc89ed49b5b8478716d1edb07fa6d45b60
Yifei He <yifei@hesyifei.com>
2021-12-21 09:02:58 -0800

    Update global.ts

--- src/global.ts
+++ src/global.ts
@@ -1,10 +1,10 @@
-// This is needed when we try to import Well Ping's type
+// This is needed when we try to import @wellping/study-schemas
 // because there `__DEV__` is defined.
 // https://stackoverflow.com/a/51240514/2603230
 // https://stackoverflow.com/a/69238076/2603230
 declare global {
   var __DEV__: boolean; // eslint-disable-line no-var
 }
-global.__DEV__ = process.env.NODE_ENV === "development";
+global.__DEV__ = true; // Always true because we want the study schema to be strictly validated.
 
 export {};
